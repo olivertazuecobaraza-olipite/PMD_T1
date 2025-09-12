eb584886cabd7289fafa606335c9dfbfa1d6eaff
olipite <oliver.tazueco.baraza@iestubalcain.net>
2025-09-12 12:20:10 +0200

    Conversion

--- src/apuntes/02_Constantes.kt
+++ src/apuntes/02_Constantes.kt
@@ -4,4 +4,6 @@ const val FICHERO : String= "Fichero.txt"
 
 fun main() {
     val name = "Oliver"
+    val fichero = "Datos.txta"
+
 }
---
+++ src/apuntes/03_ConversionTipos.kt
@@ -0,0 +1,8 @@
+package apuntes
+
+fun main() {
+    val level : Short = 75
+    val first: Int = level.toInt()
+
+    println(first)
+}
